bff4de512bbd5e8461353c6980a7d3f59b2499da
Giannis Rovithakis <giannis.rovithakis@gmail.com>
2020-01-11 19:38:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
+#define NUMBER_OF_PLAYERS 2
+
 int main(){
+	cout << " <> --- Lidl Heartstone ALPHA (v0.1) --- <>" << endl;
 
 	// Create a game board
+	cout << " > Creating Game Board..." << endl;
 	GameBoard board;
 
 	// Initialize it to prepare for the game
-	board.initializeGameBoard();
+	cout << " > Initializing Game Board..." << endl;
+	board.initializeGameBoard(NUMBER_OF_PLAYERS);
 
-	
+	// Print initial game state and statistics
+	cout << " > Printing Game Stats:" << endl;
+	board.printGameStatistics();
 
+	// Enter the main gameplay loop
+	cout << endl << " --- Starting game... ---" << endl;
+	board.gameplay();
 
+	cout << " > Quitting game..." << endl;
+	cout << " ___Game made for glory by azuran and pigeon___" << endl;
 	return 0;
 }
